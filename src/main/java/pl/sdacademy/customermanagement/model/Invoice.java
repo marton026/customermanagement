f8585c5217531775395c1427ac9da95887da839b
Marton <marton026@gmail.com>
2019-08-10 14:21:38 +0200

    create invoice test

--- src/main/java/pl/sdacademy/customermanagement/model/Invoice.java
+++ src/main/java/pl/sdacademy/customermanagement/model/Invoice.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class Invoice {
     private long id;
-    private String invoiceNo;
+    private String invoiceNo="2";
     private Date createDate;
     private Date datePaid;
 }
