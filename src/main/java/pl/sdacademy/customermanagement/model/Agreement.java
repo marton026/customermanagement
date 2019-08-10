e01e4c15936038076c08c6daedec251662970a51
Marton <marton026@gmail.com>
2019-08-10 15:08:57 +0200

    create agreement

---
+++ src/main/java/pl/sdacademy/customermanagement/model/Agreement.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.customermanagement.model;
+
+import java.util.Date;
+
+public class Agreement {
+    private long id;
+    private Date createAgrement;
+    private Date endAgreement;
+    private ListAgreement listAgreement;
+
+}
