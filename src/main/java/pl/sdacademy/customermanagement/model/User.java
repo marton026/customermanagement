f7c56e0508ee186cff4197cf75ac90af8d323514
Marton <marton026@gmail.com>
2019-08-10 23:39:06 +0200

    create user entity

--- src/main/java/pl/sdacademy/customermanagement/model/User.java
+++ src/main/java/pl/sdacademy/customermanagement/model/User.java
@@ -1,9 +1,24 @@
 package pl.sdacademy.customermanagement.model;
 
+
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class User {
 
+    @Id @GeneratedValue
     private long id;
     private String firstName;
     private String lastName;
