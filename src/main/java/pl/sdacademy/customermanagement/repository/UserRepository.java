358fde4ca92bdf21660dee9ff137caf4b68323f3
MartinSzajnog <szajnogmartin@gmail.com>
2019-08-24 20:05:24 +0200

    Item view 3

--- src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
+++ src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package pl.sdacademy.customermanagement.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.omg.CORBA.LongLongSeqHelper;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import pl.sdacademy.customermanagement.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
+
 }
