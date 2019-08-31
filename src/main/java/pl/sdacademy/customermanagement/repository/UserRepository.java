c00b55593fc41ed3dc4c644353e532c926f55d1b
Marton <marton026@gmail.com>
2019-08-31 13:55:18 +0200

    pdf generation2

--- src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
+++ src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package pl.sdacademy.customermanagement.repository;
 
 import org.omg.CORBA.LongLongSeqHelper;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 import pl.sdacademy.customermanagement.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
