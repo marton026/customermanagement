3ceb94f3680b5de25c5bfe6ab64279b14dc2bdc7
Marcin <cadi#gi*thub3759>
2019-10-25 00:16:36 +0200

    Generowanie faktur próba 1

--- src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
+++ src/main/java/pl/sdacademy/customermanagement/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package pl.sdacademy.customermanagement.repository;
 
-import org.omg.CORBA.LongLongSeqHelper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Repository;
 import pl.sdacademy.customermanagement.model.User;
 
